3a8b687f6fcb51dd83696155b9424c0abf92bb5b
enmisac <enmisac@gmail.com>
2017-11-07 19:49:24 -0600

    aplique un poco de estilos al fomrulario de contactos

--- app/Contacto/template.js
+++ app/Contacto/template.js
@@ -1,35 +1,32 @@
 const template = () => {
 	let html = `
-
-  <div class="row">
-  	<h4>Contáctanos</h4>
-    <form id="contactoForm"  class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <input name="name" id="name" type="text" class="validate">
-          <label for="name">Nombre</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input name="email" id="email" type="email" class="validate">
-          <label for="email">Correo electrónico</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s6">
-          <input name="subject" id="subject" type="text" class="validate">
-          <label for="subject">Asunto</label>
-        </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <textarea name="comments" id="comments" class="materialize-textarea" data-length="120"></textarea>
-          <label for="comments">Comentarios</label>
-        </div>
-      </div>
-      <a id="enviarContacto" class="waves-effect waves-light btn">Enviar</a>
-    </form>
+  <div class="col m10 offset-m1 s12">
+    <h4 class="center-align">Contáctanos</h4>
+    <div class="row">
+        <form class="col s12">
+          <div class="row">
+            <div class="input-field col s6">
+              <input name="name" id="name" type="text" class="validate">
+              <label for="name">Nombre</label>
+            </div>
+             <div class="input-field col s6">
+              <input name="email" id="email" type="email" class="validate">
+              <label for="email">Correo electrónico</label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s6">
+              <input name="subject" id="subject" type="text" class="validate">
+              <label for="subject">Asunto</label>
+            </div>
+            <div class="input-field col s6">
+              <textarea name="comments" id="comments" class="materialize-textarea" data-length="120"></textarea>
+              <label for="comments">Comentarios</label>
+            </div>
+          </div>
+          <a id="enviarContacto" class="waves-effect waves-light btn">Enviar</a>
+        </form>
+    </div>
   </div>
         `
   return html
