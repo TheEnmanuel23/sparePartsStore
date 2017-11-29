f7b2224855e799914afdf5cd25145b5db7ae0d6e
enmisac <enmisac@gmail.com>
2017-11-28 18:24:22 -0600

    hice que se mostrara en el detalle el asunto y no el id del asunto

--- app/EmailDetail/index.js
+++ app/EmailDetail/index.js
@@ -8,16 +8,25 @@ if (!firebase.apps.length) { firebase.initializeApp(config.firebase) }
 
 const db = firebase.database()
 
-page('/emails/:id', PreLoading, loadEmail, (ctx, next) => {
+page('/emails/:id', PreLoading, loadAsuntos, loadEmail, (ctx, next) => {
 	let content = document.querySelector('#content')
 
   let html = template(ctx.email)
   content.innerHTML = html
 })
 
+function loadAsuntos (ctx, next) {
+	firebase.database().ref('asuntos').once('value').then(snapshot => {
+		let store = snapshot.val()
+		ctx.asuntos = store
+		next()
+	})
+}
+
 function loadEmail (ctx, next) {
 	db.ref('/correosEnviados/' + ctx.params.id).once('value').then(snapshot => {
 	  let email = snapshot.val()
+	  email.subject = ctx.asuntos[email.subject].descripcion
     ctx.email = email
     next()
 	})
